f83e49f4274af83ec4f4a37debd6a669e0383372
Raz Lifshitz <raz@wavebl.com>
2020-08-20 11:46:03 +0300

    small changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import angular from 'angular'
 import envelope from './envelope/envelope.module'
 
-var mod = angular.module('app', [envelope]);
-
-export default mod;
+angular.module('app', [envelope]);
